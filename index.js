828fa47130a133f823190f33393664ca26dc0fb1
pablofullana <pablofullana@gmail.com>
2018-10-10 11:03:44 -0300

    Run Solhint for the whole watchDir on <enter>

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const chokidar = require('chokidar');
-const spawn = require('child_process').spawn;
-
-console.log('Loaded');
+const chokidar = require('chokidar')
+const spawn = require('child_process').spawn
+const keypress = require('keypress')
 
 const solhintBinary = 'solhint'
 const watchDir = './**/*.sol'
@@ -14,32 +13,36 @@ const chokidarOptions = {
   ignoreInitial: true
 };
 
+
+function runSolhint(path) {
+  console.clear()
+  console.log('Running Solhint on: %s', path)
+
+  const solhint = spawn(solhintBinary, [path])
+
+  solhint.stdout.on('data', (data) => console.log(`${data}`))
+  solhint.stderr.on('data', (data) => console.log(`${data}`))
+
+  solhint.on('close', (code) => console.log(`Solhint exited with code ${code}`))
+}
+
 function watch(options) {
-  function runSolhint(path) {
-    console.log('Running Solhint on: %s', path);
-
-    const solhint = spawn(solhintBinary, [path]);
-
-    solhint.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
-    });
-
-    solhint.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
-    });
-
-    solhint.on('close', (code) => {
-      console.log(`Solhint exited with code ${code}`);
-    });
-  }
-
   chokidar
     .watch(watchDir, chokidarOptions)
     .on(events.add, (path) => runSolhint(path))
     .on(events.change, (path) => runSolhint(path))
-    .on('error', () => console.log('error'));
+    .on('error', () => console.log('error'))
 
   console.log('Watching: %o', watchDir);
-};
+}
 
-watch();
+process.stdin.on('keypress', function (ch, key) {
+  if (key && key.name == 'enter') {
+    runSolhint(watchDir)
+  }
+});
+
+keypress(process.stdin);
+console.clear()
+console.log('Loaded')
+watch()
